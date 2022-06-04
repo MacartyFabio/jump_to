ba83ed72057e0040ea5ce52347f28509d6bed16c
Fabio Macarty <fabio.rosa@multi24h.com.br>
2022-06-03 22:20:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import jumptoImg from './assets/My_project11.png'
+import './styles.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="form-login">
+            
+            <span className="login-form-title">Bem Vindo</span>
+            <span className="login-form-title">
+              <img src={jumptoImg} alt="tela de login"/>
+            </span>
+
+            <div className="wrap-input">
+              <input type="email" className="input"/>
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input type="password" className="input" />
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button type="submit" className="login-form-btn">Login</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt_1">Não possui conta?</span>
+              <a className="txt2" href="#">Criar conta.</a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
